71b5ef28421ca7e186f0bd49c52d95f911f6784c
Saevus96 <kp.chlewski@gmail.com>
2019-01-22 18:27:40 +0100

    Projekt Modelowanie i symulacje

---
+++ misie pandy/main.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<cmath>
+#include<ctime>
+
+
+int main() {
+
+	//Symulowanie 2000 liczb metod¹ Monte Carlo 
+
+	for (int i = 0; i < 2000; i++) {
+		int numberOfBears = 0;
+
+		while (numberOfBears > 0) {
+
+		}
+
+
+	}
+
+
+	system("pause");
+	return 0;
+}
